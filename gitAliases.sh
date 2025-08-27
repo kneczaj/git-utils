38ceb58ec11795a1c654d0dfac0fc3fd597d8881
Kamil Neczaj <kneczaj@gmail.com>
2025-08-27 18:14:34 +0200

    add wip alias

--- gitAliases.sh
+++ gitAliases.sh
@@ -4,3 +4,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source ${SCRIPT_DIR}/aliases/standard.sh
 source ${SCRIPT_DIR}/aliases/originBased.sh
 source ${SCRIPT_DIR}/aliases/noVerify.sh
+source ${SCRIPT_DIR}/aliases/wip.sh
