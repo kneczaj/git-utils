2ecb78104ce467bab43b79e7511000e24bae6b9e
Kamil Neczaj <kneczaj@gmail.com>
2025-08-27 18:14:34 +0200

    Improve file structure

---
+++ sourceAll.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "${SCRIPT_DIR}/sourced/gitAliases.sh"
+
+# git aliases overwrites SCRIPT_DIR
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "${SCRIPT_DIR}/sourced/path.sh"
--- sourced/path.sh
+++ sourced/path.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # adds all the tools to path
-PATH=${SCRIPT_DIR}/tools/:$PATH
+PATH=${SCRIPT_DIR}/../visible/:$PATH
