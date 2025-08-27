ba87caf9f15541bcc175aaf339fd7949ae74cf7e
Kamil Neczaj <kneczaj@gmail.com>
2025-08-27 18:14:34 +0200

    Use camel case naming

---
+++ gitAliases.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+source ${SCRIPT_DIR}/aliases/standard.sh
+source ${SCRIPT_DIR}/aliases/originBased.sh
