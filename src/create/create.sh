3a44dfff2c6059322bbe07b373616e249ba4b107
Kamil Neczaj <kneczaj@gmail.com>
2025-08-27 18:14:34 +0200

    feat: use templates at git create script

--- src/create/create.sh
+++ src/create/create.sh
@@ -2,14 +2,24 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 set -e
-NAME=${1}
+name=${1}
+template=${2}
 
-mkdir "${NAME}"
-cd "${NAME}"
+availableTemplates="base node"
+
+if ! echo "$availableTemplates" | grep -wq "$template"; then
+  echo "usage: <dir name> <template>, where template is one of: ${availableTemplates}"
+  exit 1
+fi
+
+mkdir "${name}"
+cd "${name}"
 git init
 touch LICENSE
 git add LICENSE
 git commit -m 'Initial commit'
-cp -f ${SCRIPT_DIR}/.gitignore.node ./.gitignore
-git add .gitignore
-git commit -m 'Gitignore content'
+
+git remote add template git@github.com:kneczaj/template-base-ts.git
+git fetch template
+git merge --squash --allow-unrelated-histories template/${template}
+git commit -m "feat: use template ${template}"
