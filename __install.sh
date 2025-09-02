2a9e35b63a6de9c26cb091b6e762882474494667
Kamil Neczaj <kamil.neczaj@lr.org>
2025-09-02 17:02:14 +0200

    feat: add differentiation in casing at git install

--- __install.sh
+++ __install.sh
@@ -2,5 +2,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 set -e
 git config --global submodule.recurse true
+# differentiate in letter casing
+git config --global core.ignorecase false
 "${SCRIPT_DIR}/src/conflicts/__install.sh"
 npm install -g vite-node
